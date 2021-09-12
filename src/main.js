23be0dd4cdb766f07bfac5aed21e2a2d022a8878
duyl97 <dl97.app@gmail.com>
2021-09-12 16:44:46 +0700

    update hsb

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('combined'));
+
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => res.render('home'));
+
+app.listen(port, (req, res) => console.log(`Server listens at http://localhost:${port}`));
