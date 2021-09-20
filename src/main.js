3f6a625bb96bea3e2efd72fd7a115ac934cfd364
duyl97 <dl97.app@gmail.com>
2021-09-21 00:14:50 +0700

    update Sep 21

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 const path = require('path');
-
+const methodOverride = require('method-override');
 const route = require('./routes');
 const db = require('./config/db');
 
@@ -14,10 +14,14 @@ db.connect();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 
+app.use(methodOverride('_method'))
 // app.use(morgan('combined'));
 
 app.engine('hbs', handlebars({
-    extname: '.hbs'
+    extname: '.hbs',
+    helpers: {
+        sum: (a, b) => a + b,
+    }
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
